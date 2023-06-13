ea0f4ec1b6aa6c4a3ffba054b8f786235b86b283
naomicherono <nonniecheronoh12@gmail.com>
2023-06-13 11:49:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const block=50;
+function distanceFromHqInBlocks(block) {
+    return Math.abs(block - 42);
+
+  }
+  
+  
+  function distanceFromHqInFeet(block) {
+    return distanceFromHqInBlocks(block) * 264;
+  }
+  
+  function distanceTravelledInFeet(startBlock, endBlock) {
+    return Math.abs(startBlock - endBlock) * 264;
+  }
+  
+  function calculatesFarePrice(startBlock, endBlock) {
+    const distance = distanceTravelledInFeet(startBlock, endBlock);
+    
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else if (distance > 2500) {
+      return 'cannot travel that far';
+    }
+  }
